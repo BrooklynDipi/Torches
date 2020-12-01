c806da00134ba7194773ef6a9ce5b910923f426e
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2020-12-01 01:35:53 -0600

    POC Commit

---
+++ src/Torches/Assets/Scripts/LightController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject light;
+
+    public int oilPowerUp;
+    public int lfPowerUp;
+
+    public Vector3 scaleTransform;
+    public Vector3 standardScaleTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ScoreManager.oilTimerStart == false)
+        {
+            light.transform.localScale = standardScaleTransform;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Oil")
+        {
+            ScoreManager.oilTimeLeft =+ oilPowerUp;
+            ScoreManager.oilTimerStart = true;
+            light.transform.localScale = scaleTransform;
+        }
+
+        if (col.gameObject.tag == "Lighter Fluid")
+        {
+            ScoreManager.timeLeft = ScoreManager.timeLeft + lfPowerUp;
+        }
+    }
+}
---
+++ src/Torches/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static float timeLeft;
+    public static float oilTimeLeft;
+    public Text timerText, oilTimer;
+
+    public static bool oilTimerStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 90;
+        oilTimeLeft = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.text = "Time Remaining: " + ScoreManager.timeLeft.ToString();
+        }
+
+        if (oilTimerStart)
+        {
+            oilTimeLeft -= Time.deltaTime;
+            oilTimer.text = "Oil Burn Remaining: " + ScoreManager.oilTimeLeft.ToString();
+        }
+
+        if (oilTimeLeft <= 0)
+        {
+            oilTimeLeft = 0;
+            oilTimerStart = false;
+        }
+
+    }
+}
